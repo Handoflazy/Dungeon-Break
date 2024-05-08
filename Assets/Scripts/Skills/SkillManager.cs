57bde3603b69fb5ba5b95961842f13460d5b31e6
MinhThien27 <118949161+MinhThien27@users.noreply.github.com>
2024-05-09 00:31:16 +0700

    Skill_done
    9-5-2024

--- Assets/Scripts/Skills/SkillManager.cs
+++ Assets/Scripts/Skills/SkillManager.cs
@@ -1,36 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UIElements;
 [RequireComponent(typeof(Duration))]
+[RequireComponent(typeof(SkillBarController))]
 public class SkillManager : PlayerSystem
 {
     public BoolEvent OnUsingMoveSkill;
-    [field: SerializeField]
-    public Weapon CurrentWeapon { get; set; }
-
     private Duration duration;
+    private SkillBarController controller;
 
-    public WeaponType weaponType;
+    public AbstractSkill moveSkill;
+    public AbstractSkill firstSkill;
+    public AbstractSkill secondSkill;
 
-    public IMoveSkill iMoveSkill;
+    public bool isMoveSkillCooldown = false;
+    public bool isFirstSkillCooldown = false;
+    public bool isSecondSkillCooldown = false;
 
-    private AbstractSkill moveSkill;
-    private AbstractSkill firstSkill;
-    private AbstractSkill secondSkill;
+    private void Start()
+    {
+        HandleLevelUp();
+    }
 
-    private bool isMoveSkillCooldown;
-    private bool isFirstSkillCooldown;
-    private bool isSecondSkillCooldown;
+    private void Update()
+    {
 
-
-
+    }
     protected override void Awake()
     {
         base.Awake();
         duration = GetComponent<Duration>();
+        controller = GetComponent<SkillBarController>();
     }
     private void OnEnable()
     {
@@ -66,27 +70,36 @@ public class SkillManager : PlayerSystem
 
         if (level >= 5 && !isMoveSkillAdded)
         {
-            moveSkill = gameObject.AddComponent<DashSkill>();
+            moveSkill = GetComponent<DashSkill>();
+            if (moveSkill == null) { print("chua lay dc"); }
             isMoveSkillAdded = true;
+            moveSkill.canUse = true;
+            controller.skillBox1.gameObject.SetActive(true);
         }
 
         if (level >= 10 && !isFirstSkillAdded)
         {
-            firstSkill = gameObject.AddComponent<Grenade>();
+            firstSkill = GetComponent<MineSkill>();
+            if(firstSkill == null) { print("chua lay dc"); }
             isFirstSkillAdded = true;
+            firstSkill.canUse = true;
+            controller.skillBox2.gameObject.SetActive(true);
         }
 
         if (level >= 15 && !isSecondSkillAdded)
         {
-            secondSkill = gameObject.AddComponent<EnhanceSkill>();
+            secondSkill = GetComponent<EnhanceSkill>();
+            if (secondSkill == null) { print("chua lay dc"); }
             isSecondSkillAdded = true;
+            secondSkill.canUse = true;
+            controller.skillBox3.gameObject.SetActive(true);
         }
     }
 
     public void OnMoveSkillUse()
     {
-        if (!moveSkill)
-            return;
+        if (!moveSkill) return;
+
         int durationCost = moveSkill.GetDurationCost();
         if (durationCost < duration.CurrentDuration && !isMoveSkillCooldown)
         {
@@ -95,11 +108,24 @@ public class SkillManager : PlayerSystem
             StartCoroutine(StartMoveSkillCooldown(moveSkill.GetCowndown()));
         }
     }
+
+    public void OnFirstSkillUse()
+    {
+        if (firstSkill == null) return;
+
+        int durationCost = firstSkill.GetDurationCost();
+        if (durationCost < duration.CurrentDuration && !isFirstSkillCooldown)
+        {
+            firstSkill.OnUsed();
+            duration.ReduceDuration(durationCost);
+            StartCoroutine(StartFirstSkillCooldown(firstSkill.GetCowndown()));
+        }
+    }
+
     public void OnSecondSkillUse()
     {
+        if (secondSkill == null) return;
 
-        if (secondSkill == null)
-            return;
         int durationCost = secondSkill.GetDurationCost();
         if (durationCost < duration.CurrentDuration && !isSecondSkillCooldown)
         {
@@ -108,49 +134,37 @@ public class SkillManager : PlayerSystem
             StartCoroutine(StartSecondSkillCooldown(secondSkill.GetCowndown()));
         }
     }
-    public void OnFirstSkillUse()
-    {
-
-        if (firstSkill == null)
-            return;
-        int durationCost = firstSkill.GetDurationCost();
-        if (durationCost < duration.CurrentDuration && !isFirstSkillCooldown)
-        {
-
-            firstSkill.OnUsed();
-            duration.ReduceDuration(durationCost);
-            StartCoroutine(StartFirstSkillCooldown(firstSkill.GetCowndown()));
-        }
-    }
     IEnumerator StartMoveSkillCooldown(float timeCD)
     {
         isMoveSkillCooldown = true;
+        
+        StartCoroutine(controller.UpdateCDMoveSkill(timeCD));
         yield return new WaitForSeconds(timeCD);
-        isMoveSkillCooldown = false;
 
+        isMoveSkillCooldown = false;
     }
     IEnumerator StartFirstSkillCooldown(float timeCD)
     {
         isFirstSkillCooldown = true;
+
+        StartCoroutine(controller.UpdateCDFirstSkill(timeCD));
         yield return new WaitForSeconds(timeCD);
+
         isFirstSkillCooldown = false;
     }
 
     IEnumerator StartSecondSkillCooldown(float timeCD)
     {
         isSecondSkillCooldown = true;
+
+        StartCoroutine(controller.UpdateCDSecondSkill(timeCD));
         yield return new WaitForSeconds(timeCD);
+
         isSecondSkillCooldown = false;
     }
 
     public void OnLevelChangeEvent()
     {
         HandleLevelUp();
-    }
-
-    private void Start()
-    {
-        HandleLevelUp();
-        isMoveSkillCooldown = false;
     }
 }
