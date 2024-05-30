a66daf5dd7c493e74b1c5e0b305d5c08707ab79f
MinhThien27 <118949161+MinhThien27@users.noreply.github.com>
2024-05-31 01:29:45 +0700

    Gop Thien + Khiem
    31_5

--- Assets/Scripts/ActiveSpawnEnemy.cs
+++ Assets/Scripts/ActiveSpawnEnemy.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class ActiveSpawnEnemy : MonoBehaviour
 {
     public GameObject spawnEnemy;
+    public GameObject fenceToTrue;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") || collision.gameObject.layer == LayerMask.NameToLayer("Player") || collision.gameObject.layer == LayerMask.NameToLayer("PlayerMovermentCollider"))
         {
             spawnEnemy.SetActive(true);
+            fenceToTrue.SetActive(true);
         }
     }
     //private void OnTriggerExit2D(Collider2D collision)
